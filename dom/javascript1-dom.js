ceecb19bf1447f37f71145fae2ddcc1f4a45f057
KyuSahm <gusami32@gmail.com>
2021-09-26 18:51:03 +0900

    childNodes and children nodes

--- dom/javascript1-dom.js
+++ dom/javascript1-dom.js
@@ -1,3 +1,17 @@
+//Ex5 : childNodes를 이용한 노드 선택
+window.addEventListener("load", function() {        
+    var section5 = document.querySelector("#section5");
+    //var inputs = section5.querySelectorAll("input");
+    var box = section5.querySelector(".box");
+    //var input1 = box.childNodes[0];
+    //var input2 = box.childNodes[1];
+    var input1 = box.children[0];
+    var input2 = box.children[1];
+
+    input1.value = "hello";
+    input2.value = "okay";
+});
+
 //Ex4 : Selectors API Level1 - Name
 window.addEventListener("load", function() {        
     var section4 = document.getElementById("section4");
@@ -15,7 +29,6 @@ window.addEventListener("load", function() {
         txtSum.value = x + y;
     };
 });
-
 
 //Ex3 : Selectors API Level1 - Class
 window.addEventListener("load", function() {        
